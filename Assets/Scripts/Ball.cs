789d0db8b663e8877c88b49197a6ba4f34853d79
nrcoover <81087345+nrcoover@users.noreply.github.com>
2022-07-01 07:37:31 -0400

    Adds Movement to the Ball
    Created a script to give the ball a constant motion that also reflects that motion when colliding with a contact point of an object. (The .normal is referencing the perpendicular to of the object surface plane on collision.)

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    float _speed = 20f;
+    Rigidbody _rigidbody;
+    Vector3 _velocity;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.velocity = Vector3.down * _speed;
+    }
+
+    //Runs at 100 hertz in Unity configuration (100 times a second) for Physics based actions.
+    void FixedUpdate()
+    {
+        _rigidbody.velocity = _rigidbody.velocity.normalized * _speed;
+        _velocity = _rigidbody.velocity;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        _rigidbody.velocity = Vector3.Reflect(_velocity, collision.contacts[0].normal);
+    }
+}
