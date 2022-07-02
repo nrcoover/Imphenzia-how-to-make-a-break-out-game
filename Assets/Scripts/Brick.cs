28d71a348e137823f18479118d753f5306c04444
nrcoover <81087345+nrcoover@users.noreply.github.com>
2022-07-01 23:55:47 -0400

    Adds Flash effect on Brick upon Impact

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -8,10 +8,17 @@ public class Brick : MonoBehaviour
     public int points = 100;
     public Vector3 rotator;
     public float rotationSpeed;
+    public Material crackedMaterial;
+    Material _orgMaterial;
+    // The renderer is responsible for getting the material onto the screen.
+    Renderer _renderer;
 
     void Start()
     {
         transform.Rotate(rotator * (transform.position.x + transform.position.y * rotationSpeed));
+        _renderer = GetComponent<Renderer>();
+        // this is a shared material because shared materials increase the speed.
+        _orgMaterial = _renderer.sharedMaterial;
     }
 
     void Update()
@@ -27,5 +34,14 @@ public class Brick : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        _renderer.sharedMaterial = crackedMaterial;
+
+        Invoke("RestoreMaterial", 0.1f);
+    }
+
+    void RestoreMaterial()
+    {
+        _renderer.sharedMaterial = _orgMaterial;
     }
 }
