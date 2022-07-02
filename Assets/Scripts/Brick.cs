c4adf26ad048c0666054b1a5e3790c2a98017987
nrcoover <81087345+nrcoover@users.noreply.github.com>
2022-07-01 23:43:24 -0400

    Adds Rotation and Extra Bricks into Scene

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -6,15 +6,17 @@ public class Brick : MonoBehaviour
 {
     public int hits = 1;
     public int points = 100;
+    public Vector3 rotator;
+    public float rotationSpeed;
 
     void Start()
     {
-        
+        transform.Rotate(rotator * (transform.position.x + transform.position.y * rotationSpeed));
     }
 
     void Update()
     {
-        
+        transform.Rotate(rotator * Time.deltaTime);
     }
 
     private void OnCollisionEnter(Collision collision)
